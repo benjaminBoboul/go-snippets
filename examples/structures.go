57b116ba9f2582122a0293b1d252df571561f488
Benjamin Boboul <benjamin.boboul@protonmail.com>
2021-11-04 18:47:19 +0100

    :sparkles: Add embedded structure example

--- examples/structures.go
+++ examples/structures.go
@@ -14,10 +14,20 @@ type Person struct {
 	Addr Address
 }
 
+type EmbeddedPerson struct {
+	Address
+	Name string
+	Age int
+}
+
 func main() {
 	var p Person
 	p.Name = "Bob"
 	p.Addr.city = "Rennes"
 
 	fmt.Printf("%v", p)
+
+	var ep EmbeddedPerson
+	ep.Name = "Sarah"
+	ep.city = "Los Angeles"
 }
