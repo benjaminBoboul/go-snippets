98f582740ce5d2a76c96953a77a0958ad57faefd
Benjamin Boboul <benjamin.boboul@protonmail.com>
2021-11-12 17:44:46 +0100

    :sparkles: Add string format for Rect struct with specific receiver
    When rect is printed, it will call String method to properly format output

--- examples/receiver.go
+++ examples/receiver.go
@@ -10,6 +10,10 @@ func (r Rect) Area() int {
 	return r.Height * r.Width
 }
 
+func (r Rect) String() string {
+	return fmt.Sprintf("Rect(w=%v, h=%v)", r.Width, r.Height)
+}
+
 func main() {
 	var r Rect = Rect{2, 3}
 	fmt.Printf("Rect area=%v\n", r.Area())
